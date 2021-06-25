817367bdb36bc4869b2d97947e63eca8f22cdc88
Maximilian Müller <63018152+mxmueller@users.noreply.github.com>
2021-06-25 23:05:06 +0200

    added google analytics

--- laravel/resources/views/layouts/backbone.blade.php
+++ laravel/resources/views/layouts/backbone.blade.php
@@ -9,6 +9,16 @@
 
 <script src="{{ asset('js/app.js') }}"></script>
 
+ <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-DM1YFRCHP3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-DM1YFRCHP3');
+</script>
+
 <script>
     window.CookieConsent.init({
       // More link URL on bar
